6346d355f7161797f4f6903bd33c7280fd0f8103
Tim Beckett <timbeckett@ymail.com>
2019-06-09 05:19:35 -0400

    Set up basic api, connect to Mongo DB.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express'); 
+const connectDB = require('./config/db'); 
 
 const app = express(); 
+
+// Connects Database
+connectDB(); 
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Personal Link Saver App' })); 
 
