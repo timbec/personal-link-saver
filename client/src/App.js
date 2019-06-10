388889bec09f29551d5a9087c5d73b9d6b04710c
Tim Beckett <timbeckett@ymail.com>
2019-06-10 12:33:05 -0400

    Started basic app layout.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-dom'; 
+import Navbar from './components/layouts/Navbar';
+
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-
+          <Navbar />
       </header>
     </div>
   );
